0d43f9b45661bf29c3819c2d297dcb5d2a8eeb95
david vanslambrouck <vanslambrouckd@users.noreply.github.com>
2015-03-20 13:00:37 +0100

    bootstrap test

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->    
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
+    <style type="text/css">
+    	#header {
+    		margin-top: 30px;
+    	}
+
+    	#header-logo {
+    		margin:0px auto;
+    	}
+		#searchForm {
+			padding-top:55px;
+		}
+
+		#headerTekst {
+			font-family: 'Open Sans', serif;
+		}
+
+		h1 {
+			color:#00adef;
+			font-size: 20px;
+		}
+
+		#nav-main {
+			background-color: #2F2F2F;
+			margin-top: 30px;
+			margin-bottom: 30px;
+		}
+
+		#nav-main > div {
+			padding:0px;
+		}
+
+		#nav-main a {
+			color:#fff;
+		}
+
+		#nav-main a:hover {
+			background-color: #00adef;			
+		}
+
+		.btn-default {
+			background-color:#cfd0d2;
+			border-color:#c2c3c6;
+			color:#515153;
+		}
+
+		.btn-default:hover {
+			background-color: #babbbe;
+    		border-color: #a2a4a8;
+    		color: #515153
+    	}
+
+    	.form-control {
+    		border-color:#c2c3c6;
+    	}		
+	</style>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <div class="container">
+		<header id="header">
+		 	<div class="row">
+				<div class="col-xs-12 col-sm-3 col-md-3">
+					<img src="http://www.kindvriendelijkestedenengemeenten.be/images/layout/logo.png" id="header-logo" class="img-responsive" />
+				</div>
+				<div class="col-xs-12 col-sm-9 col-md-6" id="headerTekst">
+
+				<h1>Kindvriendelijke steden en gemeenten</h1>
+
+				<p>Op deze website vind je informatie over hoe je bouwt aan een kind- en jeugdvriendelijke stad of gemeente, een lokaal bestuur dat streeft naar de realisatie van alle rechten van het kind zoals omschreven in het Kinderrechtenverdrag.</p>
+
+				</div>
+				<div class="col-xs-9 col-xs-push-3">
+					<form class="form-inline" id="searchForm">
+						<div class="input-group">
+						  <input type="text" class="form-control" id="searchval" placeholder="zoekwaarde" />                          
+						  <span class="input-group-btn">
+						    <button type="submit" class="btn btn-default" id="btn-search">zoeken</button>
+						  </span>
+						</div>
+					</form>
+				</div>
+			</div>
+		</header>
+		<nav id="nav-main" class="nav navbar-default">
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="#">Kindvriendelijke steden en gemeenten<span class="caret"></span></a></li>
+					<li><a href="#">In 4 stappen naar het label<span class="caret"></span></a></li>
+					<li><a href="#">Toolkit &amp; methodieken<span class="caret"></span></a></li>					
+					<li><a href="#">Tips &amp; docs<span class="caret"></span></a></li>					
+					<li><a href="#">Contact<span class="caret"></span></a></li>					
+				</ul>
+			</div>
+		</nav>
+        
+      <div class="row">
+        <div class="col-md-2">
+			
+        </div>
+        <div class="col-md-7">
+			<h1>Visie op kindvriendelijkheid</h1>
+			<h2>Een kindvriendelijke stad of gemeente streeft doorheen haar volledige beleid naar respect voor de rechten van het kind.</h2>
+			
+			<p>
+	Een kindvriendelijke stad of gemeente is een gemeenschap die zicht ertoe verbindt de rechten van het kind te realiseren en te implementeren. Tegelijk kan kindvriendelijkheid gehanteerd worden als barometer voor goed lokaal beleid dat de leefbaarheid voor iedereen verhoogt. De lat voor beleid leggen op niveau van de meest kwetsbare burgers, o.a. kinderen, maakt dat je beleid iedereen in ogenschouw neemt. Kindvriendelijkheid is dan een toets voor een open, toegankelijke en open samenleving.
+			</p>
+
+			<ul>
+				<li>In een kindvriendelijke stad of gemeente, zijn kinderen en jongeren  volwaardige medeburgers en krijgen ze de kans om deel uit te maken van de stad of gemeente, er mee over na te denken, mee vorm te geven.</li>
+				<li>Kindvriendelijkheid gaat over alle levensdomeinen van kinderen en jongeren en heeft dus consequenties voor alle beleidsdomeinen. Een kindvriendelijk beleid is met andere woorden een horizontaal beleid en werkt integraal.</li>
+				<li>De implementatie van kinderrechten zijn geen doel ‘an sich’, wel kunnen ze gehanteerd worden als hefboom voor verandering. Daarenboven moeten kinderrechten steeds binnen de reële context gezien worden. Kindvriendelijkheid is een dynamisch, veranderlijk proces waarin steeds nieuwe aspecten en uitdagingen worden toegevoegd.</li>
+			</ul>
+        </div>
+        <div class="col-md-3">
+			
+        </div>
+      </div>
+    </div>
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+    /* <![CDATA[ */
+    $(document).ready(function() {
+		var site = {
+			init: function() {
+				
+			}
+		}
+		site.init();
+    });
+    /* ]]> */
+    </script>
+  </body>
+</html>
